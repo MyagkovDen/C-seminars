feaf32b4f00f03d4ab82efd199d63299abda8d9a
Denis Myagkov <tomich-84@mail.ru>
2022-11-11 21:58:36 +0300

    add ex3 дни недели

--- ex0/Program.cs
+++ ex0/Program.cs
@@ -2,11 +2,11 @@
 Console.Write("Введите целое число и нажмите Enter: ");
 string userInput = Console.ReadLine() ?? ""; // ?? "" - защита от null
 
-// 2. Преобразовать стоку в число
-int number = int.Parse(userInput); // интерполяция строк. Помним об ошибках и исключениях
+// 2. Преобразовать строку в число
+int number = int.Parse(userInput); //  Помним об ошибках и исключениях
 
 // 3. озводим число в квадрат
 int result = number * number; 
 
 // 4. Выводим результат в консоль
-Console.WriteLine($"{number} -> {result}");
+Console.WriteLine($"{number} -> {result}"); // интерполяция строк.
