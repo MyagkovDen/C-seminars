51fe53a69de057c79901e9b0bcf2af0c1e0d8fc4
Denis Myagkov <tomich-84@mail.ru>
2022-11-12 00:45:41 +0300

    ex0 performed

--- ex0/Program.cs
+++ ex0/Program.cs
@@ -3,10 +3,18 @@ Console.Write("Введите целое число и нажмите Enter: ");
 string userInput = Console.ReadLine() ?? ""; // ?? "" - защита от null
 
 // 2. Преобразовать строку в число
-int number = int.Parse(userInput); //  Помним об ошибках и исключениях
+int number = 0;
+try
+{
+    number = int.Parse(userInput); //  Помним об ошибках и исключениях
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Ошибка ввода! {ex.Message}");
+}
 
-// 3. озводим число в квадрат
-int result = number * number; 
+// 3. Возводим число в квадрат
+int result = number * number;
 
 // 4. Выводим результат в консоль
 Console.WriteLine($"{number} -> {result}"); // интерполяция строк.
