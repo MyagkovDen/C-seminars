6f6544ad354f746e61d857f777f332ddd0903a14
Denis Myagkov <tomich-84@mail.ru>
2022-11-11 21:22:24 +0300

    add ex0

---
+++ ex0/Program.cs
@@ -0,0 +1,12 @@
+﻿// 1. Получить строку, содержащую число
+Console.Write("Введите целое число и нажмите Enter: ");
+string userInput = Console.ReadLine() ?? ""; // ?? "" - защита от null
+
+// 2. Преобразовать стоку в число
+int number = int.Parse(userInput); // интерполяция строк. Помним об ошибках и исключениях
+
+// 3. озводим число в квадрат
+int result = number * number; 
+
+// 4. Выводим результат в консоль
+Console.WriteLine($"{number} -> {result}");
