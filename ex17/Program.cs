4d9f856fa5552ab26f0e97791c463d34f571ff28
Denis Myagkov <tomich-84@mail.ru>
2022-11-19 01:09:11 +0300

    ex17 formatted and amended

--- ex17/Program.cs
+++ ex17/Program.cs
@@ -1,27 +1,27 @@
 ﻿Console.Clear();
-Console.Write("Введите X: ");
-int X = int.Parse(Console.ReadLine());
+int X, Y;
 
-Console.Write("Введите Y: ");
-int Y = int.Parse(Console.ReadLine());
+try
+{
+    Console.Write("Введите X: ");
+    X = int.Parse(Console.ReadLine() ?? "");
+
+    Console.Write("Введите Y: ");
+    Y = int.Parse(Console.ReadLine() ?? "");
+}
+catch (Exception exc)
+{
+    Console.WriteLine($"Ошибка ввода данных! {exc.Message}");
+    return;
+}
 
 if (X > 0 && Y > 0)
-{
     Console.WriteLine("1");
-}
 else if (X < 0 && Y > 0)
-{
     Console.WriteLine("2");
-}
 else if (X < 0 && Y < 0)
-{
     Console.WriteLine("3");
-}
 else if (X > 0 && Y < 0)
-{
     Console.WriteLine("4");
-}
 else
-{
     Console.WriteLine("Ошибка! Точка попадает на оси координат!");
-}
