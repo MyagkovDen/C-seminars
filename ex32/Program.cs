19ab209609d357e48f8cbbeaadf10efa54185606
Denis Myagkov <tomich-84@mail.ru>
2022-11-26 00:55:16 +0300

    add ex32 and ex33

---
+++ ex32/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Clear();
+
+int[] array = GetArray(12, -9, 9);
+Console.WriteLine(String.Join(" ", array));
+
+ChangeNumber(array);
+Console.WriteLine(String.Join(" ", array));
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+// Заменяет положительные элементы массива на отрицательные, и наоборот
+
+void ChangeNumber(int[] arr)
+{
+    for (int index = 0; index < arr.Length; index++)
+    {
+        arr[index] = arr[index] * (-1);
+    }
+}
+
+
---
+++ ex33/Program.cs
@@ -0,0 +1,64 @@
+﻿// Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве
+
+Console.Clear();
+
+int figure = GetNumberFromUser("Введите целое число: ", "Ошибка ввода!");
+
+int dimensions = GetNumberFromUser("Введите длину массива: ", "Ошибка ввода!");
+int min = GetNumberFromUser("Введите значение минимального элемента массива: ", "Ошибка ввода!");
+int max = GetNumberFromUser("Введите значение максимального элемента массива: ", "Ошибка ввода!");
+int[] array = GetArray(dimensions, min, max);
+
+Console.Write($"{figure}; массив [{String.Join(" ", array)}] -> ");
+
+if (FindNumber(array, figure))
+    Console.Write("да");
+else
+    Console.Write("нет");
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Получает число от пользователя
+
+int GetNumberFromUser(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out int userNumber))
+            return userNumber;
+        else
+            Console.WriteLine(errorMessage);
+    }
+}
+
+// Возвращает массив из size элементов,
+// заполненный случайными числами из промежутка [minValue, maxValue]
+
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        res[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return res;
+}
+
+
+bool FindNumber(int[] arr, int number)
+{
+    foreach (int el in arr)
+    {
+        if (el == number) return true;
+        // else return false;
+    }
+    return false;
+}
+
+
+
+
