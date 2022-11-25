2c0f5af0df8d5d0d24bc5891c33da3c24432ae2c
Denis Myagkov <tomich-84@mail.ru>
2022-11-26 01:08:47 +0300

    add ex35

---
+++ ex35/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задайте одномерный массив из 123 
+
+Console.Clear();
+
+
+int[] array = GetArray(123, 0, 199);
+Console.WriteLine(String.Join(" ", array));
+
+int count = CountNumber(array);
+Console.WriteLine();
+
+Console.WriteLine(count);
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+// Возвращает массив из size элементов,
+
+// заполненный случайными числами из промежутка [minValue, maxValue]
+
+
+
+
+
+int[] GetArray(int size, int minValue, int maxValue)
+
+{
+
+    int[] res = new int[size];
+
+
+    for (int i = 0; i < size; i++)
+
+    {
+
+        res[i] = new Random().Next(minValue, maxValue + 1);
+
+    }
+
+    return res;
+
+}
+
+
+// Возвращает сумму положительных чисел массива arr
+
+
+int CountNumber(int[] arr)
+{
+    int count = 0;
+    for (int index = 0; index < arr.Length; index++)
+    {
+        if (arr[index] >= 10 && arr[index] <= 99)
+            count += 1;
+    }
+return count;
+}
+
+
