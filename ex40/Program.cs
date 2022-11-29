d1176ef540a5289c3fff4b67c7c3b2a0bf827fee
Denis Myagkov <tomich-84@mail.ru>
2022-11-29 23:05:19 +0300

    add ex40

---
+++ ex40/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 40. Напишите программу, которая принимает на вход три числа и проверяет, может ли существовать 
+// треугольник со сторонами такой длины.
+
+Console.Clear();
+Console.WriteLine("Введите длину стороны a");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину стороны b");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите длину стороны c");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (IfTriangle(a, b, c))
+    Console.WriteLine("Треугольник может существовать");
+else
+    Console.WriteLine("Треугольник не может существовать");
+
+//////////////////////////////////////////////////////////////////////////////////////////////////
+
+bool IfTriangle(int num1, int num2, int num3)
+{
+    return num1 < num2 + num3 && num2 < num3 + num1 && num3 < num1 + num2;
+}
