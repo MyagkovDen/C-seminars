d5d41f0f419ccaf57fe8c205112aa1e6d634e7e1
Denis Myagkov <tomich-84@mail.ru>
2022-12-03 00:40:57 +0300

    add ex51

---
+++ ex51/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив. Найдите сумму элементов, находящихся на главной диагонали 
+// (с индексами (0,0), (1,1) и т. д.)
+
+Console.Clear();
+
+Console.Write("Введите количество строк массива: ");
+int rows = int.Parse(Console.ReadLine() ?? "");
+Console.Write("Введите количество столбцов массива: ");
+int columns = int.Parse(Console.ReadLine() ?? "");
+
+int[,] array = GetArray(rows, columns, 0, 10);
+int sum = SumWithEqualIndexes(array);
+
+PrintArray(array);
+Console.WriteLine($"-> {sum}");
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////
+int[,] GetArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return result;
+}
+
+int SumWithEqualIndexes(int[,] arr)
+{
+    int sum = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+                sum += arr[i, j];
+        }
+    }
+    return sum;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            if (i == j) Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write($"{inArray[i, j]} ");
+            Console.ResetColor();
+        }
+        Console.WriteLine();
+    }
+}
